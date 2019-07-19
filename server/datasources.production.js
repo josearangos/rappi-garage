50de34f9cccd34075cb2a0f7f4ed6cb926333192
josearangos <jose.arangos@udea.edu.co>
2019-07-19 00:08:44 -0500

    add aws S3

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,6 +1,13 @@
 module.exports = {
-    db: {
-        url: process.env.DATABASE_URL,
-        connector: 'postgresql'
-    }
-}
+  db: {
+    url: process.env.DATABASE_URL,
+    connector: "postgresql"
+  },
+  amazonS3: {
+    name: "amazonS3",
+    connector: "loopback-component-storage",
+    provider: "amazon",
+    key: "UVqE2nu/Gk/lefkYzaLL5i4NUaFTQhZo78e552hv",
+    keyId: "AKIAIABKZ2SFDURFTUMA"
+  }
+};
