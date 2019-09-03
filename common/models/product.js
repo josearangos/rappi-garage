8631d2679ca4943411236e96e33e422b53c32615
josearangos <jose.arangos@udea.edu.co>
2019-09-02 23:15:24 -0500

    add quey search with skip,limit and only field of summary product

--- common/models/product.js
+++ common/models/product.js
@@ -1,35 +1,37 @@
 "use strict";
 
 module.exports = function(Product) {
-  Product.search = function(query, callback) {
+  Product.search = function(query, skip, limit, callback) {
     const filter = {
-      'where': {
-        'or': [
-          {'name': {'regexp': `${query}/i`}},
-          {'description': {'regexp': `${query}/i`}},
-        ],
+      where: {
+        or: [
+          { name: { regexp: `${query}/i` } },
+          { description: { regexp: `${query}/i` } }
+        ]
       },
+      limit: limit,
+      skip: skip,
+      fields: { id: true, name: true, price: true, photos: true }
     };
 
     Product.find(filter, function(_, products) {
       callback(null, products);
-    }
-    );
+    });
   };
 
-  Product.remoteMethod(
-    'search', {
-      http: {
-        path: '/search',
-        verb: 'get',
-      },
-      accepts: [
-        {arg: 'query', type: 'string'},
-      ],
-      returns: {
-        arg: 'search',
-        type: 'array',
-      },
+  Product.remoteMethod("search", {
+    http: {
+      path: "/search",
+      verb: "get"
+    },
+    accepts: [
+      { arg: "query", type: "string" },
+      { arg: "skip", type: "number" },
+      { arg: "limit", type: "number" }
+    ],
+    returns: {
+      arg: "search",
+      type: "array"
     }
-  );
+  });
 };
